34fca47d0eb37ffb6462a5f62e9c9453065d87d9
ScottL <sjlteacher@gmail.com>
2017-10-07 16:39:01 -0400

    Passes 7/8 rspec tests.

--- 01-data-structures/05-hashes-part-2/open_addressing/open_addressing.rb
+++ 01-data-structures/05-hashes-part-2/open_addressing/open_addressing.rb
@@ -7,12 +7,26 @@ class OpenAddressing
 
   def []=(key, value)
     i = index(key, size)
-    resize unless @items[i] == nil || @items[i].value == value
+    if @items[i] != nil
+      i = next_open_index(index(key, size))
+      if i == -1
+        resize
+        i = next_open_index(index(key, size))
+      end
+    end
     @items[i] = Node.new(key, value)
   end
 
   def [](key)
-    @items[index(key, size)].value
+    i = index(key, size)
+    while @items[i].key != key
+      if i == size
+        i = 0
+      else
+        i += 1
+      end
+    end
+    @items[i].value
   end
 
   # Returns a unique, deterministically reproducible index into an array
@@ -21,11 +35,28 @@ class OpenAddressing
   def index(key, size)
     x = 0
     key.each_codepoint { |c| x += c }
-    (x * key.length) % size
+    x % size
   end
 
   # Given an index, find the next open index in @items
   def next_open_index(index)
+    count = 0
+
+
+    i = index
+    while @items[i] != nil && i < count
+      if i == size
+        i = 0
+      else
+        i += 1
+      end
+      count += 1
+    end
+    if count == i
+      return -1
+    else
+      return i 
+    end
   end
 
   # Simple method to return the number of items in the hash
