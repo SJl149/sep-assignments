7c756de8e6ae683a9c472005ff18cd6dc3ff504d
ScottL <sjlteacher@gmail.com>
2017-10-08 13:01:21 -0400

    Separate_chaining.rb passes all specs.

--- 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
@@ -5,30 +5,48 @@ class SeparateChaining
 
   def initialize(size)
     @max_load_factor = 0.7
-    @item = Array.new(size)
+    @items = Array.new(size)
   end
 
   def []=(key, value)
     i = index(key, size)
-    resize unless @items[i] == nil || @items[i].value == value
-    # @items[i] = Node.new(key, value)
+    if @items[i] == nil
+      @items[i] = LinkedList.new
+    end
+    @items[i].add_to_front(Node.new(key, value))
+    resize if load_factor > @max_load_factor
   end
 
   def [](key)
-    @items[index(key, size)].value
+    list_item = @items[index(key, size)].head
+    while list_item.key != key
+      list_item = list_item.next
+    end
+    list_item.value
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
-    x = 0
-    key.each_codepoint { |c| x += c }
-    (x * key.length) % size
+    x = key.each_codepoint.sum
+    x % size
   end
 
   # Calculate the current load factor
   def load_factor
+    items_count = 0
+    @items.each do |list|
+      unless list == nil
+        item = list.head
+        items_count += 1
+        until item.next == nil
+          item = item.next
+          items_count += 1
+        end
+      end
+    end
+    items_count * 1.0 / size
   end
 
   # Simple method to return the number of items in the hash
@@ -40,9 +58,17 @@ class SeparateChaining
   def resize
     temp_items = @items
     @items = Array.new(size * 2)
-    temp_items.each do |item|
-      unless item == nil
-        @items[index(item.key, size)] = item
+    temp_items.each do |list|
+      unless list == nil
+        while list.head
+          item = list.head
+          i = index(item.key, size)
+          if @items[i] == nil
+            @items[i] = LinkedList.new
+          end
+          @items[i].add_to_front(Node.new(item.key, item.value))
+          list.remove_front
+        end
       end
     end
   end
