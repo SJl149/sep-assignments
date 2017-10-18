0f658ccb78999eac8f29c7a8bc2f7d4545572ffa
ScottL <sjlteacher@gmail.com>
2017-10-18 17:07:08 -0400

    Added some data for the bacon.

--- 01-data-structures/07-graph/graph_bacon.rb
+++ 01-data-structures/07-graph/graph_bacon.rb
@@ -8,14 +8,28 @@ class Bacon
 
   def find_bacon(node)
     films_to_bacon = []
-
+    dist = 0
+    queue = []
     # Loop through node.film_actor_hash
-    # Loop through films in film_actor_hash
-      # If actor is Bacon end
-      # Else place in queue with distance
-      # place in adjacency list with distance
-      # when all films looped through, then queue.deq node and cycle through add dist += 1
+    node.film_actor_hash.each do | film, actors |
+      dist += 1
 
+      # Loop through films in film_actor_hash
+      actors.each do | actor |
+
+        # If actor is Bacon end
+        if actor.name = "kevin_bacon"
+          # Return the Bacon and Filmname
+        else
+          # Else place in queue with distance
+          queue << actor
+
+          # place in adjacency list with distance
+          films_to_bacon << actor
+        end
+      end
+    end
+    # when all films looped through, then queue.deq node and cycle through add dist += 1
   end
 
 end
