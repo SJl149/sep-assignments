f34736c62e18f2154b72d90320fcb7f40a758945
ScottL <sjlteacher@gmail.com>
2017-10-22 16:25:20 -0400

    Still trying to debug.

--- 01-data-structures/07-graph/graph_bacon.rb
+++ 01-data-structures/07-graph/graph_bacon.rb
@@ -11,35 +11,45 @@ class Bacon
     actors_visited = []
     node_stack = [node]
     film_stack = []
+    film_list = [[],[],[],[],[],[],[]]
     found = false
 
     until found do
       curr_node = node_stack.pop
       children = []
-      films = []
-      
-      #return false if curr_node == nil
+      dist += 1
+
       if curr_node == nil || curr_node.name == 'kevin_bacon'
         found = true
       else
         curr_node.film_actor_hash.each do | film, actors |
 
-          unless film_stack.include?(film)
-            films << film
+          if bacon?(actors)
             film_stack << film
-            actors.each do | actor |
-              unless actors_visited.include?(actor)
-                children << actor
-                actors_visited << actor
+            break
+          else
+            unless film_list.flatten.include?(film)
+              film_list[dist] << film
+              film_stack << film
+              actors.each do | actor |
+                unless actors_visited.include?(actor)
+                  children << actor
+                  actors_visited << actor
+                end
               end
             end
           end
         end
-        dist += 1
         node_stack = node_stack + children
       end
     end
     return film_stack
   end
 
+end
+
+def bacon?(node_array)
+  includes = false
+  node_array.each { |actor| includes = true if actor.name = 'kevin_bacon' }
+  includes
 end
