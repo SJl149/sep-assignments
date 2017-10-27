566c8458d2df071f2a2768ed109308eb2aca5a98
ScottL <sjlteacher@gmail.com>
2017-10-26 21:44:13 -0400

    Close but caught in endless loop.

--- 01-data-structures/07-graph/graph_bacon.rb
+++ 01-data-structures/07-graph/graph_bacon.rb
@@ -8,25 +8,39 @@ class Bacon
 
   def find_bacon(node)
     return [] unless node && node.name != "kevin_bacon"
-    actors_visited = []
     queue = [node]
-    bacon = false
-    connections = []
-
-    while (queue.length != 0) && bacon == false
+    film_list = [[],[],[],[],[],[]]
+    dist = 0
+    node.dist = 0
+    while (queue.length != 0) && dist < 7
       curr_node = queue.shift
       curr_node.film_actor_hash.each do | film, actors |
-        if !connections.include?(film)
-          connections << film
+        if !film_list.flatten.include?(film)
+          film_list[dist] << film
         end
         actors.each do | actor |
-          if unvisited?(queue, actor, actors_visited)
+          if actor.dist == nil
+            actor.dist = dist + 1
+            actor.predecessor = curr_node
             queue << actor
-            actors_visited << actor.name
           end
           if actor.name == "kevin_bacon"
-            bacon = true
-            return connections
+            if actor.predecessor == node
+              return [film]
+            end
+            path = []
+            child = actor
+            while child.predecessor != nil do
+              parent = child.predecessor
+              parent.film_actor_hash.each do |movie, cast|
+                if cast.include?(child)
+                  path << movie
+                end
+              end
+              parent = child.predecessor
+              dist -= 1
+            end
+            return path
           end
         end
       end
@@ -34,41 +48,4 @@ class Bacon
     return []
   end
 
-  def unvisited?(queue, actor, actors_visited)
-    actor.name != "kevin_bacon" && !queue.include?(actor) && !actors_visited.include?(actor.name)
-  end
-
-  def backtrack(kb, node)
-    kb.film_actor_hash.each do | film, actors |
-
-
-    end
-  end
-
-  def create_adj_list(node)
-    # Create adjacency list to keep track of films and actors
-    # Once Bacon is retrace back to source
-    # Else if dist > 6 or film_actor_hash for actor is empty or films have all been visited, then delete film from connections
-    dist = 1
-    queue = [node]
-    films_visited = []
-    actors_visited = []
-
-    while queue.size != 0 || dist < 7
-      curr_node = queue.shift
-      curr_node.film_actor_hash.each do |film, actors|
-        if film && !films_visited.include?(film)
-          @graph[dist] << film
-          films_visited << film
-          actors.each do |actor|
-            if !actors_visited.include?(actor.name)
-              queue << actor
-              actors_visited << actor.name
-            end
-          end
-        end
-      end
-      dist += 1
-    end
-  end
 end
--- 01-data-structures/07-graph/node.rb
+++ 01-data-structures/07-graph/node.rb
@@ -1,9 +1,13 @@
 class Node
   attr_accessor :name
   attr_accessor :film_actor_hash
+  attr_accessor :dist
+  attr_accessor :predecessor
 
   def initialize(name)
     @name = name
     @film_actor_hash = {}
+    @dist = nil
+    @predecessor = nil
   end
 end
