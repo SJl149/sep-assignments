56b416953b6b4da619f26084ae711b97a8a582b9
ScottL <sjlteacher@gmail.com>
2017-10-29 15:15:56 -0400

    Sorting algorithms completed.

--- 02-algorithms/03-algorithms-sorting/sort.rb
+++ 02-algorithms/03-algorithms-sorting/sort.rb
@@ -63,6 +63,62 @@ def heapify(heap, i, max)
 end
 
 # Bucket Sort
-def bucket_sort(collection)
+def bucket_sort(collection, bucket_size = 10)
   return collection if collection.length <= 1
+  min = collection.min
+  max = collection.max
+
+  # Create buckets
+  bucket_count = ((max.ord - min.ord) / bucket_size).floor + 1
+  buckets = Array.new(bucket_count)
+  (0..buckets.length - 1).each { |i| buckets[i] = [] }
+
+  # Place in buckets
+  (0..collection.length - 1).each { |i| buckets[((collection[i].ord - min.ord) / bucket_size).floor].push(collection[i]) }
+
+  # Sort buckets and return
+  collection.clear
+  (0..buckets.length - 1).each do |i|
+    buckets[i] = insertion_sort(buckets[i])
+    buckets[i].each { |value| collection.push(value) }
+  end
+  collection.reject!{ |value| value == nil }
+  collection
+end
+
+def insertion_sort(collection)
+  sorted = [collection.delete_at(0)]
+  for val in collection
+    sorted_index = 0
+    while sorted_index < sorted.length
+      if val <= sorted[sorted_index]
+        sorted.insert(sorted_index, val)
+        break
+      elsif sorted_index == sorted.length - 1
+        sorted.insert(sorted_index + 1, val)
+        break
+      end
+      sorted_index += 1
+    end
+  end
+  sorted
+end
+
+# Benchmark the quick_sort, heap_sort, and bucket_sort.
+test_collection = (0..10000).to_a.sample(50)
+puts " "
+puts "Benchmark tests for quick_sort, heap_sort, and bucket_sort"
+puts " "
+print test_collection
+puts " "
+Benchmark.bm do |bm|
+  bm.report('quick_sort') do
+    quick_sort(test_collection)
+  end
+  bm.report('heap_sort') do
+    heap_sort(test_collection)
+  end
+  bm.report('bucket_sort') do
+    bucket_sort(test_collection)
+  end
 end
