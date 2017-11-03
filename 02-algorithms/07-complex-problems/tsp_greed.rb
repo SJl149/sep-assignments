ae22edabbc1d2ef9dc5b36d75c833260ff8a4a7a
ScottL <sjlteacher@gmail.com>
2017-11-03 15:27:54 -0400

    Nearest neighbor created.

--- 02-algorithms/07-complex-problems/tsp_greed.rb
+++ 02-algorithms/07-complex-problems/tsp_greed.rb
@@ -1,14 +1,15 @@
-DEF nearest possible neighbor(graph_of_cities, current_city)
-   WHILE current_city.visited IS FALSE
-     SET neighbor_cities TO current_city.neighbors
-     SET next_city = neighbor_cities[0]
 
-     FOR current_neighbor IN neighbor_cities
-       IF current_neighbor.distance < next_city.distance
-         ASSIGN current_neighbor TO next_city
-       END IF
-     END FOR
-     SET current_city.visited to TRUE
-     ASSIGN next_city TO current_city
-   END WHILE
- END DEF
+def nearest_neighbor(graph_of_cities, current_city)
+  while current_city == false
+    neighbor_cities = current_city.neighbors
+    next_city = neighbor_cities[0]
+
+    for current_neighbor in neighbor_cities
+      if current_neighbor.distance < next_city.distance
+        current_neighbor = next_city
+      end
+    end
+    current_city.visited = true
+    next_city = current_city
+  end
+end
