0d4119419f8e72330d27b6bea64cc70633722b45
ScottL <sjlteacher@gmail.com>
2017-11-05 18:15:17 -0500

    #find_route gets caught in endless loop.

--- 02-algorithms/07-complex-problems/tsp_greed.rb
+++ 02-algorithms/07-complex-problems/tsp_greed.rb
@@ -4,30 +4,97 @@ class Tsp
 
   include Math
 
+  attr_accessor :city_list
 
+  def initialize()
+    @city_list = [
+      [[],[],[],[],[]],
+      [[],[],[],[],[]],
+      [[],[],[],[],[]],
+      [[],[],[],[],[]],
+      [[],[],[],[],[]],
+      [[],[],[],[],[]],
+      [[],[],[],[],[]],
+      [[],[],[],[],[]]
+    ]
+    # 8x5 matrix of long(60-130 by 10) and lat(25-45 by 5)
+  end
 
-  def nearest_neighbor(graph_of_cities, current_city)
-    while current_city == false
-      neighbor_cities = current_city.neighbors
-      next_city = neighbor_cities[0]
-
-      for current_neighbor in neighbor_cities
-        if current_neighbor.distance < next_city.distance
-          current_neighbor = next_city
-        end
-      end
-      current_city.visited = true
-      next_city = current_city
+  def add_cities(city_nodes)
+    city_nodes.each do |city_node|
+      long = (city_node.long / 10).floor - 6
+      lat = (city_node.lat / 5).floor - 5
+      @city_list[long][lat] << city_node
     end
+  end
+
+  def city_neighbors(city_node)
+    long = (city_node.long / 10).floor - 6
+    lat = (city_node.lat / 5).floor - 5
+    @city_list[long][lat]
+  end
+
+  def find_route(cities)
+    # add to distance matrix
+    add_cities(cities)
+    # set current_city and neighbors
+    neighbors = cities
+    current_city = cities.shift
+    route = [current_city]
+    @city_list.delete(current_city)
+
+    while neighbors.count != 1 do
+      next_city = nearest_neighbor(neighbors, current_city)
+      route << neighbors.delete(next_city)
+      @city_list.delete(current_city)
+      current_city = next_city
+    end
+    route << neighbors.shift
+    route
+    # find closest neighbor for each city
+    # find shortest route
+  end
+
+  def nearest_neighbor(cities, current_city)
+    # get neighbor_cities from @city_list
+    long = (current_city.long / 10).floor - 6
+    lat = (current_city.lat / 5).floor - 5
+    neighbor_cities = @city_list[long][lat]
+    while neighbor_cities.empty? do
+      if long < 6
+        neighbor_cities = @city_list[long + 1][lat]
+      end
+      if long > 0
+        neighbor_cities = @city_list[long - 1][lat]
+      end
+      if lat < 5
+        neighbor_cities = @city_list[long][lat + 1]
+      end
+      if lat > 0
+        neighbor_cities = @city_list[long][lat - 1]
+      end
+    end
+
+    # find nearest city
+    shortest = 10000
+    nearest_city = nil
+    neighbor_cities.each do |neighbor|
+      dist = calc_dist(current_city, neighbor)
+      if dist < shortest
+        nearest_city = neighbor
+      end
+    end
+    nearest_city
   end
 
   private
 
-  def calc_dist(lat1, long1, lat2, long2)
+  def calc_dist(city1, city2)
+
     radius_mi = 3959
-    deg_lat = deg_to_rad(lat2 - lat1)
-    deg_long = deg_to_rad(long2 - long1)
-    a = sin(deg_lat / 2) * sin(deg_lat / 2) + cos(deg_to_rad(lat1)) * cos(deg_to_rad(lat2)) * sin(deg_long / 2) * sin(deg_long / 2)
+    deg_lat = deg_to_rad(city2.lat - city1.lat)
+    deg_long = deg_to_rad(city2.long - city1.long)
+    a = sin(deg_lat / 2) * sin(deg_lat / 2) + cos(deg_to_rad(city1.lat)) * cos(deg_to_rad(city2.lat)) * sin(deg_long / 2) * sin(deg_long / 2)
     c = 2 * atan2(sqrt(a), sqrt(1 - a))
     radius_mi * c
   end
---
+++ 02-algorithms/07-complex-problems/tsp_greed_spec.rb
@@ -0,0 +1,65 @@
+include RSpec
+
+require_relative 'tsp_greed'
+require_relative 'city'
+
+RSpec.describe Tsp, type: Class do
+  let (:new_york) { City.new("New York", 40.7, 74) }
+  let (:los_angeles) { City.new("Los Angeles", 34, 118.2) }
+  let (:chicago) { City.new("Chicago", 41.8, 87.6) }
+  let (:houston) { City.new("Houston", 29.8, 95.4) }
+  let (:philadelphia) { City.new("Philadelphia", 40, 75.2) }
+  let (:phoenix) { City.new("Phoenix", 33.4, 112.1) }
+  let (:san_antonio) { City.new("San Antonio", 29.4, 98.5) }
+  let (:san_diego) { City.new("San Diego", 32.7, 117.2) }
+  let (:dallas) { City.new("Dallas", 32.8, 96.8) }
+  let (:san_jose) {City.new("San Jose", 37.3, 121.9)}
+  let (:austin) {City.new("Austin", 30.3, 97.7)}
+  let (:indianapolis) {City.new("Indianapolis", 39.8, 86.2)}
+  let (:jacksonville) {City.new("Jacksonville", 30.3, 81.7)}
+  let (:san_francisco) {City.new("San Francisco", 37.8, 122.4)}
+  let (:columbus) {City.new("Columbus", 40, 83)}
+  let (:charlotte) {City.new("Charlotte", 35.2, 80.8)}
+  let (:detroit) {City.new("Detroit", 42.3, 83)}
+  let (:memphis) {City.new("Memphis", 35.1, 90)}
+  let (:seattle) {City.new("Seattle", 47.6, 122.3)}
+  let (:denver) {City.new("Denver", 39.7, 105)}
+  let (:washington) {City.new("Washington DC", 38.9, 77)}
+  let (:boston) {City.new("Boston", 42.4, 71.1)}
+  let (:oklahoma_city) {City.new("Oklahoma City", 35.5, 97.5)}
+  let (:portland) {City.new("Portland", 45.5, 122.7)}
+  let (:las_vegas) {City.new("Las Vegas", 36.2, 115.1)}
+  let (:milwaukee) {City.new("Milwaukee", 43, 87.9)}
+  let (:kansas_city) {City.new("Kansas City", 39.1, 94.6)}
+  let (:atlanta) {City.new("Atlanta", 33.7, 84.4)}
+  let (:omaha) {City.new("Omaha", 41.3, 96)}
+  let (:miami) {City.new("Miami", 25.8, 80.2)}
+
+  let (:lowman) { Tsp.new() }
+
+  describe "#add_city" do
+    it "adds a city node to the matrix with the right longitude and latitude" do
+      lowman.add_cities([new_york])
+      nyc_long = (new_york.long / 10).floor - 6
+      nyc_lat = (new_york.lat / 5).floor - 5
+      expect(lowman.city_list[nyc_long][nyc_lat]).to include(new_york)
+    end
+  end
+
+  describe "#nearest_neighbor" do
+    it "returns the nearest neighbor city nodes" do
+      cities = [new_york, los_angeles, boston, miami, philadelphia]
+      lowman.add_cities(cities)
+      expect(lowman.nearest_neighbor(cities, atlanta)).to eq(miami)
+    end
+  end
+
+  describe "#find_route" do
+    it "finds an efficient route given a list of cities" do
+      cities = [new_york, los_angeles, boston, miami, philadelphia, houston, atlanta, omaha, columbus, jacksonville]
+      ideal_route = [new_york, boston, philadelphia, columbus, atlanta, jacksonville, miami, houston, omaha, los_angeles]
+      expect(lowman.find_route(cities)).to eq(ideal_route)
+    end
+  end
+
+end
