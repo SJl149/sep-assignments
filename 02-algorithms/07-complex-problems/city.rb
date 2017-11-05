6e76a498b5fae3b8145e20ffe39c7c87f8f376ef
ScottL <sjlteacher@gmail.com>
2017-11-04 20:01:23 -0400

    Created city.rb for city nodes and cities.rb as a list of cities.

---
+++ 02-algorithms/07-complex-problems/city.rb
@@ -0,0 +1,13 @@
+class City
+  attr_accessor :name
+  attr_accessor :neighbors
+  attr_accessor :lat
+  attr_accessor :long
+
+  def initialize(name, lat, long)
+    @name = name
+    @lat = lat
+    @long = long
+  end
+  
+end
--- 02-algorithms/07-complex-problems/tsp_greed.rb
+++ 02-algorithms/07-complex-problems/tsp_greed.rb
@@ -1,15 +1,34 @@
+require_relative 'City'
 
-def nearest_neighbor(graph_of_cities, current_city)
-  while current_city == false
-    neighbor_cities = current_city.neighbors
-    next_city = neighbor_cities[0]
+class Tsp
 
-    for current_neighbor in neighbor_cities
-      if current_neighbor.distance < next_city.distance
-        current_neighbor = next_city
+  include Math
+
+  def calc_dist(lat1, long1, lat2, long2)
+    radius_mi = 3959
+    deg_lat = deg_to_rad(lat2 - lat1)
+    deg_long = deg_to_rad(long2 - long1)
+    a = sin(deg_lat / 2) * sin(deg_lat / 2) + cos(deg_to_rad(lat1)) * cos(deg_to_rad(lat2)) * sin(deg_long / 2) * sin(deg_long / 2)
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    radius_mi * c
+  end
+
+  def deg_to_rad(deg)
+    deg * PI / 180
+  end
+
+  def nearest_neighbor(graph_of_cities, current_city)
+    while current_city == false
+      neighbor_cities = current_city.neighbors
+      next_city = neighbor_cities[0]
+
+      for current_neighbor in neighbor_cities
+        if current_neighbor.distance < next_city.distance
+          current_neighbor = next_city
+        end
       end
+      current_city.visited = true
+      next_city = current_city
     end
-    current_city.visited = true
-    next_city = current_city
   end
 end
